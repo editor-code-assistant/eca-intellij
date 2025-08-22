7e6f857b2f84ff66fc5b7237167040e4abb875e3
Eric Dallo <ericdallo06@hotmail.com>
2025-08-22 10:15:31 -0300

    Fix for prod

--- src/main/clojure/dev/eca/eca_intellij/extension/tool_window.clj
+++ src/main/clojure/dev/eca/eca_intellij/extension/tool_window.clj
@@ -1,10 +1,7 @@
 (ns dev.eca.eca-intellij.extension.tool-window
   (:require
-   [clojure.java.io :as io]
-   [clojure.string :as string]
    [com.github.ericdallo.clj4intellij.extension :refer [def-extension]]
    [com.github.ericdallo.clj4intellij.logger :as logger]
-   [com.rpl.proxy-plus :refer [proxy+]]
    [dev.eca.eca-intellij.config :as config]
    [dev.eca.eca-intellij.db :as db])
   (:import
@@ -15,74 +12,16 @@
    [com.intellij.ui.content ContentFactory]
    [com.intellij.ui.jcef JBCefBrowser JBCefJSQuery]
    [dev.eca.eca_intellij Icons]
-   [java.io IOException InputStream]
-   [java.net URLConnection]
-   [org.cef CefApp]
-   [org.cef.callback CefCallback CefSchemeHandlerFactory]
-   [org.cef.handler CefLoadHandler$ErrorCode CefResourceHandler]
-   [org.cef.misc IntRef]
-   [org.cef.network CefRequest CefResponse]))
+   [dev.eca.eca_intellij EcaSchemeHandlerFactory]
+   [org.cef CefApp]))
 
 (set! *warn-on-reflection* true)
-
-(defn ^:private read-response [data-out bytes-to-read ^IntRef bytes-read state*]
-  (boolean
-   (when-let [^InputStream input-stream (:input-stream @state*)]
-     (let [available (.available input-stream)
-           max-bytes (min available bytes-to-read)]
-       (if (> available 0)
-         (let [byte (.read input-stream data-out 0 max-bytes)]
-           (.set bytes-read byte)
-           true)
-         (do
-           (.close input-stream)
-           false))))))
-
-(defn ^:private response-headers [^CefResponse resp ^IntRef resp-length current-url* state*]
-  (try
-    (when-let [conn ^URLConnection (:conn @state*)]
-      (let [url (str (.getURL conn))]
-        (cond
-          (string/includes? url "css")
-          (.setMimeType resp "text/css")
-
-          (string/includes? url "js")
-          (.setMimeType resp "text/javascript")
-
-          (string/includes? url "html")
-          (.setMimeType resp "text/html")
-
-          :else
-          (.setMimeType resp (.getContentType conn))))
-
-      (.set resp-length (.available ^InputStream (:input-stream @state*)))
-      (.setStatus resp 200))
-    (catch IOException e
-      (.setStatus resp 404)
-      (.setError resp CefLoadHandler$ErrorCode/ERR_FILE_NOT_FOUND)
-      (.setStatusText resp (.getLocalizedMessage e)))))
-
-(defn ^:private process-request [^CefRequest req ^CefCallback callback state* current-url*]
-  (boolean
-   (when-let [url (.getURL req)]
-     (let [resource-path (string/replace url "http://eca" "webview")
-           new-url (io/resource resource-path (.getClassLoader clojure.lang.Symbol))
-           conn (when new-url (.openConnection new-url))
-           is (when conn (.getInputStream conn))]
-       (swap! state* assoc
-              :input-stream is
-              :conn conn)
-       (reset! current-url* new-url)
-       (.Continue callback)
-       true))))
 
 (defn ^:private create-webview ^JBCefBrowser [^Project project]
   (let [browser (-> (JBCefBrowser/createBuilder)
                     (.setOffScreenRendering true) ;; TODO move to config
                     (.build))
-        webview (JBCefJSQuery/create browser)
-        state* (atom {})
-        current-url* (atom nil)]
+        webview (JBCefJSQuery/create browser)]
     (db/assoc-in project [:webview-browser] browser)
     (Disposer/register project browser)
     (.addHandler webview (fn [msg]
@@ -91,21 +30,7 @@
      (CefApp/getInstance)
      "http"
      "eca"
-     (proxy+
-      []
-      CefSchemeHandlerFactory
-       (create [_ _browser _frame _scheme _req]
-         (proxy+ [] CefResourceHandler
-           (processRequest [_ req callback]
-             (process-request req callback state* current-url*))
-           (getResponseHeaders [_ resp length _redirect-url]
-             (response-headers resp length current-url* state*))
-           (readResponse [_ data-out bytes-to-read bytes-read _callback]
-             (read-response data-out bytes-to-read bytes-read state*))
-           (cancel [_]
-             (when-let [^InputStream is (:input-stream @state*)]
-               (.close is))
-             (swap! state* dissoc :input-stream))))))
+     (EcaSchemeHandlerFactory.))
     browser))
 
 (defn ^:private create-tool-window-content
@@ -149,7 +74,6 @@
   (getAnchor [_] ToolWindowAnchor/RIGHT))
 
 (comment
-  (.loadURL (db/get-in (first (db/all-projects)) [:webview-browser]) "http://eca/foo")
   (.loadURL (db/get-in (first (db/all-projects)) [:webview-browser]) "http://eca/index.html")
   (.loadURL (db/get-in (first (db/all-projects)) [:webview-browser]) "http://localhost:5173")
   ;; open devtools
